6843350b44f821ce32bf776ea941b2be6971b4a7
Seong Yong-ju <sei40kr@gmail.com>
2020-05-27 23:12:48 +0900

    feat: make changes with wire

--- main.go
+++ main.go
@@ -2,12 +2,18 @@
 
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+)
 
 type Message string
 
 type Greeter struct {
 	Message Message
+	Grumpy  bool
 }
 
 type Event struct {
@@ -19,15 +25,25 @@ func NewMessage() Message {
 }
 
 func NewGreeter(m Message) Greeter {
-	return Greeter{Message: m}
+	var grumpy bool
+	if time.Now().Unix()%2 == 0 {
+		grumpy = true
+	}
+	return Greeter{Message: m, Grumpy: grumpy}
 }
 
 func (g Greeter) Greet() Message {
+	if g.Grumpy {
+		return Message("Go away!")
+	}
 	return g.Message
 }
 
-func NewEvent(g Greeter) Event {
-	return Event{Greeter: g}
+func NewEvent(g Greeter) (Event, error) {
+	if g.Grumpy {
+		return Event{}, errors.New("could not create event: event greeter is grumpy")
+	}
+	return Event{Greeter: g}, nil
 }
 
 func (e Event) Start() {
@@ -37,7 +53,10 @@ func (e Event) Start() {
 }
 
 func main() {
-	e := InitializeEvent()
-
+	e, err := InitializeEvent()
+	if err != nil {
+		fmt.Printf("failed to create event: %s\n", err)
+		os.Exit(2)
+	}
 	e.Start()
 }
